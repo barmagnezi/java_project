8b4fd28761176e8ea923c201edb21b48e2efdd7b
Bar <Barmagnezi22@gmail.com>
2015-05-18 01:17:50 +0300

    update

--- java_project/src/hibernate/mazesToFromDatabase.java
+++ java_project/src/hibernate/mazesToFromDatabase.java
@@ -11,9 +11,8 @@ import java.util.List;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.service.ServiceRegistry;
+
 
 import model.StringMaze;
 import model.StringSolution;
@@ -21,17 +20,7 @@ import algorithms.mazeGenerators.Maze;
 import algorithms.search.Solution;
 
 public class mazesToFromDatabase {
-	private static SessionFactory sessionFactory;
-	private static ServiceRegistry serviceRegistry;
 
-	public static SessionFactory createSessionFactory() {
-	    Configuration configuration = new Configuration();
-	    configuration.configure();
-		serviceRegistry = new StandardServiceRegistryBuilder().applySettings(
-	            configuration.getProperties()).build();
-	    sessionFactory = configuration.buildSessionFactory(serviceRegistry);
-	    return sessionFactory;
-	}	
 
 	public void writeToDatabase(HashMap<String, Maze> nameMaze,HashMap<Maze, Solution> MazeSol){
 		
