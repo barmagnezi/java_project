08f141debf946b2d0b7def96e8cb24244179cd23
senia <senia333@gmail.com>
2015-05-17 23:56:26 +0300

    trying to hibernate.

--- java_project/src/boot/tests.java
+++ java_project/src/boot/tests.java
@@ -1,8 +1,43 @@
 package boot;
 
+import hibernate.mazesToFromDatabase;
+
+import java.util.HashMap;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import algorithms.mazeGenerators.Maze;
+import algorithms.search.Solution;
+
 public class tests {
 
 	public static void main(String[] args) {
+		 User user = new User();
+		 user.setFirstName("Kermit");
+		 user.setLastName("Frog");
+		 user.setAge(54);
+		 user.setEmail("kermit@muppets.com");
+
+		 SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory(); 
+		 Session session = sessionFactory.openSession();
+		 UserManager manager = new UserManager(session);
+
+		 manager.saveUser(user);
+		 System.out.println("User saved with ID = "+ user.getUserId());
+
+		 session.flush();
+		
+		
+		HashMap<String, Maze> nameMaze=new HashMap<>();
+		HashMap<Maze, Solution> MazeSol=new HashMap<>();
+		Maze m = new Maze(10, 10);	
+		Solution sol = new Solution();
+		nameMaze.put("senia", m);
+		MazeSol.put(m, sol);
+		mazesToFromDatabase myf = new mazesToFromDatabase();
+		myf.writeToDatabase(nameMaze, MazeSol);
 		/*Maze maze=new DFSMazeGenerator().generateMaze(100, 100);
 		String strmaze=new StringMaze().MazeToString(maze);
 		System.out.println(strmaze.length());
