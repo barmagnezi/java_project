b74eb4bef6b19c97de7ef90809eaf118febaa681
Bar <Barmagnezi22@gmail.com>
2015-05-22 18:24:15 +0300

    my Design completed

--- java_project/src/view/viewGUI/MazeViewWidget.java
+++ java_project/src/view/viewGUI/MazeViewWidget.java
@@ -7,6 +7,7 @@ import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Canvas;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
 
 public class MazeViewWidget extends Canvas {
@@ -18,17 +19,16 @@ public class MazeViewWidget extends Canvas {
 	Button Bhelp;
 	Label LBsteps;
 	Button Bstartover;
+	Canvas Maze;
+	Group GroupCharacters;
+	Group GroupBackgroundMaze;
 	
 	public MazeViewWidget(Composite parent, int style) {
 		super(parent, style);
-		load();
-
-	}
-	public void load(){
+		
 		this.setLayout(new GridLayout(2,false));
 		
 		LBmazeName=new Label(this, SWT.NONE);	
-		LBmazeName.setText("Maze name: "+mazeName);
 		LBmazeName.setLayoutData(new GridData(SWT.LEFT, SWT.None, false, false, 1, 1));
 		
 		Bhelp=new Button(this, SWT.PUSH);
@@ -36,16 +36,39 @@ public class MazeViewWidget extends Canvas {
 		Bhelp.setText("help");
 		
 		LBsteps=new Label(this, SWT.NONE);	
-		LBsteps.setText("Number of steps: "+steps);
 		LBsteps.setLayoutData(new GridData(SWT.LEFT, SWT.TOP, false, false, 1, 1));
 		
 		Bstartover=new Button(this, SWT.PUSH);
 		Bstartover.setLayoutData(new GridData(SWT.RIGHT, SWT.TOP, false, false, 1, 1));
 		Bstartover.setText("start over");
 		
-		Canvas Maze=new Canvas(this, SWT.NONE);
+		Maze=new Canvas(this, SWT.NONE);
 		Maze.setBackground(new Color(null,10,50,30));
 		Maze.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 2, 1));
+		
+		GroupCharacters=new Group(this, SWT.SHADOW_OUT);
+		GroupCharacters.setText("Characters");
+		GroupCharacters.setLayout(new GridLayout(3, true));
+		GroupCharacters.setLayoutData(new GridData(SWT.LEFT, SWT.DOWN,  false, false));
+		
+		for(int i = 0; i < 3; i++) 
+			new Button(GroupCharacters, SWT.RADIO).setText("option " + (i + 1));       
+
+		GroupBackgroundMaze=new Group(this, SWT.SHADOW_OUT);
+		GroupBackgroundMaze.setText("background");
+		GroupBackgroundMaze.setLayout(new GridLayout(3, true));
+		GroupBackgroundMaze.setLayoutData(new GridData(SWT.LEFT, SWT.DOWN,  false, false));
+		
+		for(int i = 0; i < 3; i++) 
+			new Button(GroupBackgroundMaze, SWT.RADIO).setText("option " + (i + 1));  
+		
+		load();
+
+	}
+	public void load(){
+		LBmazeName.setText("Maze name: "+mazeName);
+		LBsteps.setText("Number of steps: "+steps);
+
 	}
 
 
