f923c90214df03d7160755411ebb813206ba8b90
Bar <Barmagnezi22@gmail.com>
2015-06-11 02:25:09 +0300

    my client update

--- java_project/src/model/MyClient.java
+++ java_project/src/model/MyClient.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.io.PrintStream;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
@@ -11,6 +12,27 @@ import java.util.Scanner;
 
 public class MyClient {
 
+	Socket myServer;
+	PrintStream writer;
+	BufferedReader reader;
+	
+	public MyClient(int port, String ip) {
+		super();
+		try {
+			myServer=new Socket(ip, port);
+			writer=new PrintStream(myServer.getOutputStream());
+			reader=new BufferedReader(new InputStreamReader(myServer.getInputStream()));
+		} catch (IOException e) {
+			System.out.println("can't connected to ip:"+ip+"port:"+port);
+		}
+		
+		
+	}
+	public void send(String text){
+		writer.println(text);
+		writer.flush();
+	}
+/*
 	public static void main(String[] args) throws Exception {
 		//Client side
 		System.out.println("CLIENT side");
@@ -39,5 +61,5 @@ public class MyClient {
 			line=inFromServer.readLine();
 		}
 		System.out.println("END");
-	}
+	}*/
 }
